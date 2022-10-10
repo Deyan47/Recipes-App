8c48e7c7db6d7f91e21adb0f448e0739b025b66b
Deyan Yonkov <deyan.yonkov@elando.bg>
2022-10-10 17:53:44 +0300

    Showing each recipe

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,19 @@
+// export class Recipe {
+//   public name: string;
+//   public description: string;
+//   public imagePath: string;
+
+//   constructor(name: string, desc: string, imagePath: string) {
+//     this.name = name;
+//     this.description = desc;
+//     this.imagePath = imagePath;
+//   }
+// }
+
+// No need to use class, exept we'll use a method
+
+export interface Recipe {
+  name: string;
+  description: string;
+  imagePath: string;
+}
